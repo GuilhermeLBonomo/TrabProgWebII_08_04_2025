fc5f88670adfdce6134e8d6aa819150112481a53
GuilhermeLBonomo <Darkish5044@proton.me>
2025-05-04 21:49:59 -0300

    debug4

--- init.sql
+++ init.sql
@@ -1,2 +1,9 @@
-CREATE DATABASE mms_user;
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'mms_user') THEN
+      CREATE DATABASE mms_user;
+   END IF;
+END
+$$;
 GRANT ALL PRIVILEGES ON DATABASE mms_user TO postgres;
